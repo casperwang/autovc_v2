38ce964dde5e6ac33c89e289af5f9d8531b72ecf
xSeanliux <zhxnliu@gmail.com>
2020-04-02 11:45:27 +0800

    Added params.py

--- conversion.py
+++ conversion.py
@@ -13,45 +13,33 @@ import numpy as np
 import data.dataLoader as datas
 from math import ceil
 from model import Generator
+from params import *
 import os
 import pdb
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True' #Prevents OMP Error #15
 
-bottle_neck = 32
-dim_style = 256
-dim_pre = 512
-freq = 32
-
-batch_size = 1
-
-device = 'cpu'
-G = Generator(bottle_neck, dim_style, dim_pre, freq).eval().to(device).double()
-
-g_checkpoint = torch.load('./weights/autovc.ckpt', map_location = torch.device('cpu')) #AutoVC model weights
-G.load_state_dict(g_checkpoint['model'])
 
 data = datas.voiceDataset()
 metadata = torch.utils.data.DataLoader([data[0]], batch_size=batch_size)
 
 spect_vc = []
+def convert(model, current_iter):
+	for i, sbmt_i in enumerate(tqdm(metadata)):
+		uttr_org = sbmt_i["org_uttr"].to(device).double()
+		
+		emb_org = sbmt_i['org_enc'].to(device).double()
+		
+		emb_trg = sbmt_i["trg_enc"].to(device).double()
+		
+		with torch.no_grad():
+			_, x_identic_psnt, _ = model(uttr_org, emb_org, emb_trg)
+		
+		x_identic_psnt = x_identic_psnt.unsqueeze(0)
+		pdb.set_trace()
+		uttr_trg = x_identic_psnt[0, 0, :, :].cpu().numpy()
+		
+		spect_vc.append( ('{}x{}'.format(sbmt_i["person"].item(), sbmt_i["person"].item()), uttr_trg) )
 
-for i, sbmt_i in enumerate(tqdm(metadata)):
-    
-    uttr_org = sbmt_i["org_uttr"].to(device).double()
-    
-    emb_org = sbmt_i['org_enc'].to(device).double()
-    
-    emb_trg = sbmt_i["trg_enc"].to(device).double()
-    
-    with torch.no_grad():
-        _, x_identic_psnt, _ = G(uttr_org, emb_org, emb_trg)
-    
-    x_identic_psnt = x_identic_psnt.unsqueeze(0)
-    pdb.set_trace()
-    uttr_trg = x_identic_psnt[0, 0, :, :].cpu().numpy()
-    
-    spect_vc.append( ('{}x{}'.format(sbmt_i["person"].item(), sbmt_i["person"].item()), uttr_trg) )
-
-with open('results.pkl', 'wb') as handle:
-    pickle.dump(spect_vc, handle)
+	with open('./result_pkl/results_iter{}.pkl'.format(current_iter), 'wb') as handle:
+		pickle.dump(spect_vc, handle)
--- vocoder.py
+++ vocoder.py
@@ -6,16 +6,15 @@ import librosa
 import pickle
 from synthesis import build_model
 from synthesis import wavegen
+from params import *
 
-spect_vc = pickle.load(open('./results.pkl', 'rb'))
-device = torch.device("cpu")
 model = build_model().to(device)
 checkpoint = torch.load("checkpoint_step001000000_ema.pth", map_location = torch.device(device)) #Using the pretrained WaveNet Vocoder 
 model.load_state_dict(checkpoint["state_dict"])
 
-for spect in spect_vc:
-    name = spect[0]
-    c = spect[1]
-    print(name)
-    waveform = wavegen(model, c=c)
-    librosa.output.write_wav(name+'.wav', waveform, sr=16000)
+def genspec(pkl_path, write_name, save_dir = "./result_wav/"):
+	spect_vc = pickle.load(open(pkl_path, "rb"))
+	for spect in spect_vc:
+		c = spect[1]
+		waveform = wavegen(model, c=c)
+		librosa.output.write_wav(save_dir + write_name + '.wav', waveform, sr=16000)
